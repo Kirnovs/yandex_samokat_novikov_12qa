12bd28656e93adb7309874883722f2f5a04cdf15
Kirnovs <Kirnovs@mail.ru>
2024-01-28 16:45:21 +0500

    Update sender_requests.py
    optimize imports

--- sender_requests.py
+++ sender_requests.py
@@ -1,11 +1,10 @@
 import requests
 import config
-import data
 
 
-def create_order(create_order_body):
+def create_order(order_body):
     return requests.post(config.URL_SERVICE + config.CREATE_ORDER_PATH,
-                         json=data.create_order_body)
+                         json=order_body)
 
 
 def get_order(order_track):
